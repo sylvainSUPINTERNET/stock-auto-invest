214114092c71f9ba5314436f9f792fc296cc50a3
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-06-05 23:12:21 +0200

    [INIT]

--- Program.cs
+++ Program.cs
@@ -1,26 +1,15 @@
 using Alpaca.Markets;
 using Microsoft.Extensions.Logging;
+using System;
 
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
 try 
 
     {
-
-
-    if ( builder.Environment.IsDevelopment() ) 
-        {
-            app.Logger.LogInformation("Development environment detected - Alpaca Paper client will be used");
-        }
-
-
-    if ( builder.Environment.IsProduction() ) 
-        {
-            app.Logger.LogWarning("Production environment detected - Alpaca Live client will be used");
-        }
-
 
     String alpacaSecretKey = builder.Configuration.GetSection("Alpaca").GetSection("ApiKeySecret").Value;
     String alpacaSecretPublic = builder.Configuration.GetSection("Alpaca").GetSection("ApiKeyPublic").Value;
@@ -31,6 +20,38 @@ try
         }
 
         app.Logger.LogInformation("Alpaca API Key found in appsettings.json starting new client ...");
+
+        IAlpacaTradingClient? client = null;
+
+        if ( builder.Environment.IsDevelopment() ) 
+            {
+                app.Logger.LogInformation("Development environment detected - Alpaca Paper client will be used");
+                client = Alpaca.Markets.Environments.Paper.GetAlpacaTradingClient(new SecretKey(alpacaSecretPublic, alpacaSecretKey));
+            }
+
+
+        if ( builder.Environment.IsProduction() ) 
+            {
+                app.Logger.LogWarning("Production environment detected - Alpaca Live client will be used");
+                client = Alpaca.Markets.Environments.Live.GetAlpacaTradingClient(new SecretKey(alpacaSecretPublic, alpacaSecretKey));
+            }
+
+        if ( client == null ) 
+            {
+                throw new Exception("Alpaca client failed to be created !");
+            }
+
+
+        var clock = await client.GetClockAsync();
+
+            if (clock != null)
+            {
+                Console.WriteLine(
+                    "Timestamp: {0}, NextOpen: {1}, NextClose: {2}",
+                    clock.TimestampUtc, clock.NextOpenUtc, clock.NextCloseUtc);
+            }
+        
+
 
         app.MapGet("/", () => "Hello World!");
 
